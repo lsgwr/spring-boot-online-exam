772149e5831686c83bd34b193bac12348f9dc3ec
19920625lsg <1648266192@qq.com>
2020-10-18 09:17:52 +0800

    引入了summernote并写了个demo

--- frontend/exam/src/views/test/NoteDemo.vue
+++ frontend/exam/src/views/test/NoteDemo.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-// 引入summernote
+// 引入summernote，参考https://blog.csdn.net/qq_24734285/article/details/80246093
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
