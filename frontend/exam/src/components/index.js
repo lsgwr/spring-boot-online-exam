b2e38dc487ada2caa291c8c479fe534d784a6fbd
19920625lsg <1648266192@qq.com>
2020-10-19 23:36:49 +0800

    清理Chart组件

--- frontend/exam/src/components/index.js
+++ frontend/exam/src/components/index.js
@@ -1,16 +1,3 @@
-// chart
-import Bar from '../components/Charts/Bar'
-import ChartCard from '../components/Charts/ChartCard'
-import Liquid from '../components/Charts/Liquid'
-import MiniArea from '../components/Charts/MiniArea'
-import MiniSmoothArea from '../components/Charts/MiniSmoothArea'
-import MiniBar from '../components/Charts/MiniBar'
-import MiniProgress from '../components/Charts/MiniProgress'
-import Radar from '../components/Charts/Radar'
-import RankList from '../components/Charts/RankList'
-import TransferBar from '../components/Charts/TransferBar'
-import TagCloud from '../components/Charts/TagCloud'
-
 // pro components
 import AvatarList from '../components/AvatarList'
 import CountDown from '../components/CountDown'
@@ -31,17 +18,6 @@ import ArticleListContent from '../components/ArticleListContent'
 
 export {
   AvatarList,
-  Bar,
-  ChartCard,
-  Liquid,
-  MiniArea,
-  MiniSmoothArea,
-  MiniBar,
-  MiniProgress,
-  Radar,
-  TagCloud,
-  RankList,
-  TransferBar,
   Trend,
   CountDown,
   Ellipsis,
