817a95f07e9eb83c3ee26d655c18e773e39215d9
19920625lsg <1648266192@qq.com>
2021-11-15 22:26:51 +0800

    完善docker构建

--- make_docker.sh
+++ make_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf backend/src/main/resources/static
 rm -rf frontend/dist
-cd frontend && npm install && npm run build && mv dist ../../backend/src/main/resources/static
-cd ../../backend && mvn clean package
-cd ../../ && docker build -t waterknife/exam .
+cd frontend && npm install && npm run build && mv dist ../backend/src/main/resources/static
+cd ../backend && mvn clean package
+cd ../ && docker build -t waterknife/exam .
