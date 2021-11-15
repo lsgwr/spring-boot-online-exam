4e8ff1cdfb9222c99d77e86e5a507673a4f48cee
lsgwr <1648266192@qq.com>
2021-11-15 02:17:34 +0000

    添加目录清理

--- make_docker.sh
+++ make_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm -rf backend/exam/src/main/resources/static
+rm -rf frontend/exam/dist
 cd frontend/exam/ && npm install && npm run build && mv dist ../../backend/exam/src/main/resources/static
 cd ../../backend/exam/ && mvn clean package
 cd ../../ && docker build -t waterknife/exam .
