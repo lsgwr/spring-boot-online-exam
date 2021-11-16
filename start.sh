207371b9b5d26f9cdcc87613e9472f370e2b1df7
lsgwr <1648266192@qq.com>
2021-11-16 03:35:14 +0000

    完善脚本

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f exam
 docker run -d -p 80:9527 -p 3306:3306 --name exam --privileged=true waterknife/exam  /usr/sbin/init
-docker exec -it exam /bin/sh /lsg/entrypoint.sh
+docker exec exam /lsg/entrypoint.sh
